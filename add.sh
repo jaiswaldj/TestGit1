431616b4ff5dfa812a39794e52e0583c96b2c1ff
Deepak Jaiswal <jaiswaldj96@gmail.com>
2021-03-16 19:46:15 +0530

    [Deepak] modified add.sh

--- add.sh
+++ add.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
-
+read -p "Enter the 1st number" number
+read -p "Enter the 2nd number"number
+read -p "Enter the 3rd number"number
---
+++ read.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+read -p "enter the number"number
+read -p "enter the 2nd number"number
+read -p "enter the 3rd number"number
